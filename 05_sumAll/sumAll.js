adb92d9c91e5eeab854c0c75f80d9197fab98895
Tristan Hinsley <thinsley@protonmail.com>
2022-06-20 15:18:34 -0400

    Added initial code to pass all 6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let lowerBound;
+    let upperBound;
+    //Put arguments in the right order
+    if (typeof(firstNumber) != 'number' || typeof(secondNumber) != 'number') {
+        return 'ERROR';
+    }
+    else if(firstNumber < 0 || secondNumber < 0) {
+        return 'ERROR';
+    }
+    else if (firstNumber < secondNumber) {
+        lowerBound = firstNumber;
+        upperBound = secondNumber;
+    }
+    else if (firstNumber > secondNumber) {
+        lowerBound = secondNumber;
+        upperBound = firstNumber;
+    }
+    else {
+        return "ERROR";
+    }
 
+    let finalNumber = 0;
+    for(let i=lowerBound; i<=upperBound; i++) {
+        finalNumber += i;
+    }
+    return finalNumber;
 };
 
 // Do not edit below this line
