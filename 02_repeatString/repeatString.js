e43867ce1e6a1a65d295138e5900013b13aeeac6
Tristan Hinsley <thinsley@protonmail.com>
2022-06-20 14:10:10 -0400

    Implemented repeatString code to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, loopRepetitions) {
+    let finalString = '';
+    for(let i=0; i<loopRepetitions; i++) {
+        finalString += inputString;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
