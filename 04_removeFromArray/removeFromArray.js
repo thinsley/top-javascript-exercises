cbd1992a8224d68395ba2d1fb4ecaa990356ec49
Tristan Hinsley <thinsley@protonmail.com>
2022-06-20 14:25:50 -0400

    Add initial code for removeFromArray to pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, removalElement) {
+    finalArray = [];
+    for(let i=0; i<inputArray.length; i++) {
+        if(inputArray[i] != removalElement) {
+            finalArray.push(inputArray[i]);
+        }
+    }
 
+    return finalArray;
 };
 
 // Do not edit below this line
