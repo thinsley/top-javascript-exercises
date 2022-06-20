5b2e5c5c94974ef180ca4bda13e19a334e4bede0
Tristan Hinsley <thinsley@protonmail.com>
2022-06-20 15:27:25 -0400

    Added initial code to pass all 6 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    const testYear = inputYear;
 
+    if((testYear % 4 == 0 && testYear % 100 != 0) || testYear % 400 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
