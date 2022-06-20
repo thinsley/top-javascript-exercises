6d949930db9ee0b2f2547cfc757abfa7d1bc810f
Tristan Hinsley <thinsley@protonmail.com>
2022-06-20 14:17:12 -0400

    Add initial code to meet tests
    Added reverseString code to meet reverseString tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    inputStringLength = inputString.length;
+    finalString = '';
+    for(let i=inputStringLength; i>=0; i--) {
+        finalString += inputString.charAt(i);
+    }
 
+    return finalString;
 };
 
 // Do not edit below this line
