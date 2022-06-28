4bac3fc4ecbfc7a53e624c6c1a71658c77614a38
Tristan Hinsley <thinsley@protonmail.com>
2022-06-28 19:03:44 -0400

    Add logic to fibonacci program to pass all unit tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+    if (input < 0) return 'OOPS';
+    else if (typeof input == "string") {
+        let stringInt = parseInt(input);
+        let sum = 0, previousOne = 0, previousTwo = 1;
+        for (let n = 0; n < stringInt - 1; n++) {
+            sum = previousOne + previousTwo;
+            previousOne = previousTwo;
+            previousTwo = sum;
+        }
+        return previousTwo;
+    }
+    else {
+        let sum = 0, previousOne = 0, previousTwo = 1;
+        for (let n = 0; n < input - 1; n++) {
+            sum = previousOne + previousTwo;
+            previousOne = previousTwo;
+            previousTwo = sum;
+        }
+        return previousTwo;
+    }
 };
 
 // Do not edit below this line
