6f17c2b4508ae9921a5d7f340d5b7dc4657c30df
Tristan Hinsley <thinsley@protonmail.com>
2022-06-28 18:21:14 -0400

    Add code to pass first two palindrome tests
    Added initial code logic to meet the first two palindrome unit tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (stringToTest) {
+    let lowerString = stringToTest.toLowerCase();
+    let lowerStringArray = lowerString.split("").filter(letter => letter != "!" && letter != "," && letter != ".");
+    let reverseString = lowerStringArray.reverse().join("");
+    let challengeString = lowerStringArray.join("");
+    console.log(challengeString, reverseString);
+    if (challengeString == reverseString) {
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
