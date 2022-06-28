8ee07021883857160a5c6cacadd6112a7b89dcb9
Tristan Hinsley <thinsley@protonmail.com>
2022-06-28 17:59:06 -0400

    Add calculator code to pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first-second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	array.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+	array.forEach(element => {
+    product *= element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(integer) {
+	let answer = 1;
+  for (let i=integer; i>0; i--) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
